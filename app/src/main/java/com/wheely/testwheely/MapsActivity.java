d102fc00f625bf6ac3ed4e2d1ba78457ae47c880
sinotovilya <sinotovilya@gmail.com>
2016-03-22 12:19:36 +0300

    init

---
+++ app/src/main/java/com/wheely/testwheely/MapsActivity.java
@@ -0,0 +1,59 @@
+package com.wheely.testwheely;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.orm.SugarRecord;
+
+import java.util.List;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    public static final String action = "com.wheely.testwheely.ACTION.ADDPOINTS";
+    private final IntentFilter filter = new IntentFilter(action);
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<Point> points = SugarRecord.listAll(Point.class);
+            map.clear();
+            for (Point point : points) {
+                map.addMarker(new MarkerOptions().position(new LatLng(point.getLat(), point.getLon())));
+            }
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        registerReceiver(receiver, filter);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+}
---
+++ app/src/main/java/com/wheely/testwheely/Point.java
@@ -0,0 +1,48 @@
+package com.wheely.testwheely;
+
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Table;
+
+@Table
+public class Point{
+    private Long id;
+    private double lon;
+    private double lat;
+
+    public Point() {
+    }
+
+    public Point(Long id, double lat, double lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setLon(double lon) {
+
+        this.lon = lon;
+    }
+}
